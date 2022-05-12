00d24ff4fe0db571427582b3a7a728186a9dd272
Luca Di Rosso <57531037+dijerosso59@users.noreply.github.com>
2022-05-12 15:13:41 +0200

    Update Training.vue

--- src/components/Training.vue
+++ src/components/Training.vue
@@ -13,11 +13,13 @@ defineProps({
     required: true,
   },
 });
+
+const source = "/src/assets/images/"
 </script>
 
 <template>
   <router-link class="space-y-2 col-span-2" :to="link">
-    <img class="w-1/2 mx-auto" :src="'src/assets/images/' + image" alt="" />
+    <img class="w-1/2 mx-auto" :src="source + image" />
     <h2>{{ text }}</h2>
   </router-link>
 </template>
